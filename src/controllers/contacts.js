6ad0b7898f4e5241f499039f459d8985b14c4328
Vital3414 <qwer1234432148@gmail.com>
2025-03-26 08:12:39 +0200

    upd data contacts

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -27,7 +27,7 @@ export const getContactsController = async (req, res) => {
   res.json({
     status: 200,
     message: 'Successfully found contacts!',
-    data: contacts,
+    data: contacts || [],
   });
 };
 
