183a4638e245f6d7879607326f5eb7a0fa901e52
Vital3414 <112972483+Vital3414@users.noreply.github.com>
2025-04-09 09:23:34 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -28,12 +28,13 @@ async function setupServer() {
   );
   app.use('/auth', authRoutes);
   app.use('/contacts', authenticate, Router);
-  app.use('*', notFoundHandler);
-  app.use(errorHandler);
   app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.use('/api-docs', swaggerDocs());
-
+  
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
+  
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
